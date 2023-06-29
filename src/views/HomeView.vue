db9be5cf4bc3d0ffaa3057ccb0ea45e45b415b25
nicolasgabriiel_ <nicolasgabriel.developer@gmail.com>
2023-06-28 21:49:45 -0300

    Estilizando HomeView
    Estilizando todo o Fundo da nossa Página, por algum motivo quando rodo o projeto ele está com uma barra de uns 20px na parte superior totalmente vazia, ainda não identifiquei o problema, vou rodar ele no vercel e ver se o problema continua ou é do servidor local

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,75 @@
 <template>
-  <div class="background-fora">
-            <div class="background-dentro">
+  <div class="box-pai">
+    <div class="background-fora">
+      <div class="background-dentro">
 
-                <header><!--        Cabeçalho           -->
-                    <div class="titulo"><h1>Pokedéx</h1></div>
-                    <div class="busca_um"></div>
-                    <div class="busca_dois"> </div>
-                </header><!--        Cabeçalho           -->
-                
-                <main><!--      Corpo Principal     -->
-                </main><!--      Corpo Principal     -->
-           
-            </div><!--background-dentro-->
-        </div><!--background-fora-->
+        <header><!--        Cabeçalho           -->
+          <div class="titulo">
+            <h1>Pokedéx</h1>
+          </div>
+          <div class="busca_um"></div>
+          <div class="busca_dois"> </div>
+        </header><!--        Cabeçalho           -->
+
+        <main><!--      Corpo Principal     -->
+        </main><!--      Corpo Principal     -->
+
+      </div><!--background-dentro-->
+    </div><!--background-fora-->
+  </div>
 </template>
 
-<style scoped> 
-  /* Importando Fonte */
-  @font-face {
-    font-family: "Flexo-Regular";
-    src: url(../assets/fonts/Flexo/Flexo-Regular/Flexo-Regular.ttf) format('truetype');
+<style scoped> /* Importando Fonte */
+ @font-face {
+   font-family: "Flexo-Regular";
+   src: url(../assets/fonts/Flexo/Flexo-Regular/Flexo-Regular.ttf) format('truetype');
+ }
+
+ .box-pai {
+   font-family: "Flexo-Regular";
+ }
+
+ .background-fora {
+   width: 100%;
+   height: 100vh;
+   background-image: url(../assets//images/bg_1.png);
+
+ }
+
+ .background-dentro {
+   width: 70vw;
+   height: 100vh;
+   background-color: white;
+   background-image: url(../assets//images/bg_2.png);
+   margin: 0px auto;
+ }
+
+ /*           Header           */
+ .titulo {
+   width: 40vw;
+   background-color: white;
+   margin: 0 auto;
+ }
+
+ .titulo h1 {
+   font-size: 30px;
+   color: #9F9191;
+   font-weight: 400;
+   padding: 25px 0 25px 20px;
+ }
+ .busca_um{
+    width:70vw;
+    height: 80px;
+    background-color:#616161 ;
+    margin:0 auto;
 }
-  .home h1{
-    font-family: "Flexo-Regular";
-  }
+.busca_dois{
+    width:20vw;
+    height: 18px;
+    background-color:#616161 ;
+    margin:0 auto;
+    border-radius: 0 0 18px 18px;
+}
 </style>
 
 <script lang="ts">
