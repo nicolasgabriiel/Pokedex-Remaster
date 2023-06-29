16e9aa1c86266094b5be42a8f128139c5bcaafb1
nicolasgabriiel_ <nicolasgabriel.developer@gmail.com>
2023-06-28 21:29:53 -0300

    Estruturando HomeView
    Montando o Esqueleto da Página Inicial, deixando tudo pronto pra estilização do Background e Cabeçalho

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="home">
-    <h1>home</h1>
-  </div>
+  <div class="background-fora">
+            <div class="background-dentro">
+
+                <header><!--        Cabeçalho           -->
+                    <div class="titulo"><h1>Pokedéx</h1></div>
+                    <div class="busca_um"></div>
+                    <div class="busca_dois"> </div>
+                </header><!--        Cabeçalho           -->
+                
+                <main><!--      Corpo Principal     -->
+                </main><!--      Corpo Principal     -->
+           
+            </div><!--background-dentro-->
+        </div><!--background-fora-->
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  name: 'HomeView',
-});
-</script>
 
 <style scoped> 
   /* Importando Fonte */
@@ -22,3 +25,13 @@ export default defineComponent({
     font-family: "Flexo-Regular";
   }
 </style>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HomeView',
+});
+</script>
+
+
