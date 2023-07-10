da9b44bac16492a5de952fe08270e370e57bba2d
nicolasgabriiel_ <nicolasgabriel.developer@gmail.com>
2023-07-10 20:17:42 -0300

    Botões de troca
    Modificando Lógica de funcionamento dos botões de troca de pokemon nas páginas individauis

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
@@ -18,5 +19,24 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes
 })
+
+let isReloading = false
+
+router.beforeEach((to, from, next) => {
+  if (to.query.reload === 'true' && !isReloading) {
+    isReloading = true
+    // Recarrega a rota programaticamente
+    router.replace({ ...to, query: { ...to.query, reload: undefined } }).then(() => {
+      isReloading = false
+      window.location.reload()
+    })
+  } else {
+    next() // Permite a navegação normalmente
+  }
+})
+
+
+
+
 
 export default router
